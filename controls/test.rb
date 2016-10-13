927a49f0fb5927081f4fac26545824be34d410ed
Christoph Hartmann <chris@lollyrock.com>
2016-10-13 13:26:39 +0100

    update example control
    Signed-off-by: Christoph Hartmann <chris@lollyrock.com>

--- controls/test.rb
+++ controls/test.rb
@@ -3,6 +3,16 @@ control "vmware-1" do
   impact 0.7
   title 'Checks that soft power off is diabled'
   describe vmware_vm_advancedsetting({datacenter: 'ha-datacenter', vm: 'testvm'}) do
-    its('softPowerOff') { should eq 'false' }
+    its('softPowerOff') { should cmp 'false' }
   end
 end
+
+# control "vmware-1" do
+#   vmware.datacenter.each { |dc|
+#     dc.vms.each { |vm|
+#       describe vmware_vm_advancedsetting({datacenter: dc.name, vm: vm.name}) do
+#         its('softPowerOff') { should cmp 'false' }
+#       end
+#     }
+#   }
+# end
