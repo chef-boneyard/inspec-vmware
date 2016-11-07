78027cc5456c2ae792ea14f8d669ae4fbb0ba70a
Christoph Hartmann <chris@lollyrock.com>
2016-11-07 17:28:50 -0500

    let handle resources cases where no connection is available
    Signed-off-by: Christoph Hartmann <chris@lollyrock.com>

--- libraries/esx_conn.rb
+++ libraries/esx_conn.rb
@@ -2,9 +2,17 @@ class ESXConnection
   def initialize
     # TODO: this does not belong here and should be an InSpec target information
     # can I extend this from InSpec
-    # INSPEC_ESX_CONN='vsphere://root:vmwarevmware@192.168.10.139'
-    connection = URI(ENV['INSPEC_ESX_CONN'])
 
+    require 'rbvmomi'
+
+    # INSPEC_ESX_CONN='vsphere://root:vmwarevmware@192.168.10.139'
+    connection_string = ENV['INSPEC_ESX_CONN']
+    if connection_string.nil?
+      puts 'Please use vsphere://username:password@host'
+      return
+    end
+
+    connection = URI(connection_string)
     if connection.scheme != 'vsphere' ||
        connection.host.nil? ||
        connection.password.nil? ||
@@ -18,11 +26,12 @@ class ESXConnection
       password: connection.password,
       insecure: true,
     }
-    require 'rbvmomi'
   end
 
   def connection
     return @conn if defined?(@conn)
     @conn = RbVmomi::VIM.connect @conn_opts
+  rescue RuntimeError
+    nil
   end
 end
